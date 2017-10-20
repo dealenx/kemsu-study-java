8b2504cb14e080d424d99d9a781ec5b21c8bdb98
Daniil Gorodiloff <dealenx@gmail.com>
2017-10-20 21:37:59 +0700

    0.0.3

--- app/GeometricApp.java
+++ app/GeometricApp.java
@@ -8,7 +8,7 @@ import dealenx.geometry.figures.three_dimensional.*;
 public class GeometricApp {
 
 	public static void main(String[] args) {
-
+/*
 		try {
 			Circle circle = new Circle();
 			System.out.println( "MY Circle" +  circle.getRadius() );
@@ -20,6 +20,6 @@ public class GeometricApp {
         System.out.println("Error code:" + ex.getErrorCode());
         System.out.println("Error message:" + ex.getMessage());
     }
-
+*/
 	}
 }
