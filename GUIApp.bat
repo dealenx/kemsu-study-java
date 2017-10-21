32ea46bd8cf3411b9b0a7b100076bdd0c401157e
Daniil Gorodiloff <dealenx@gmail.com>
2017-10-21 10:45:32 +0700

    add GUIApp

---
+++ GUIApp.bat
@@ -0,0 +1,5 @@
+chcp 1251
+
+"C:\Program Files\Java\jdk1.8.0_144\bin\javac.exe" .\app\GUIApp.java
+"C:\Program Files\Java\jre1.8.0_144\bin\java.exe" -cp .;.. app.GUIApp Circle
+pause
---
+++ app/GUIApp.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUIApp {
+
+	public static void main(String[] args) {
+
+	}
+}
